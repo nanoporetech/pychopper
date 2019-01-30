33de9905040c9a696ad38bd885e7bc0747ed56a2
Botond Sipos <Botond.Sipos@nanoporetech.com>
2019-01-30 14:11:52 +0000

    Bump version: 0.2.0 → 0.3.0

--- pychopper/__init__.py
+++ pychopper/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'ONT Applications Group'
 __email__ = 'Apps@nanoporetech.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='pychopper',
-    version='0.2.0',
+    version='0.3.0',
     description="TODO.",
     long_description=readme,
     author="ONT Applications Group",
