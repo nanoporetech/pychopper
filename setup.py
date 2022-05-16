0dd905b6cd903656eb4dfb9dfd27c2544f807305
Neil Horner <neil.horner@nanoporetech.com>
2022-05-16 17:19:52 +0100

    Update setp.py to be more like aplanat's. Remove pypi step as we wonke be making it (depends on parasail, which is not in pypi)

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@
 
 from setuptools import setup
 from glob import glob
+import re
+import os
+import pkg_resources
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
@@ -24,9 +27,35 @@ test_requirements = [
     # TODO: put package test requirements here
 ]
 
+__pkg_name__ = 'pychopper'
+__author__ = 'cwright'
+__description__ = 'Identify, orient, and trim full-length ONT cDNA reads.'
+
+__path__ = os.path.dirname(__file__)
+__pkg_path__ = os.path.join(os.path.join(__path__, __pkg_name__))
+# Get the version number from __init__.py
+verstrline = open(os.path.join(__pkg_name__, '__init__.py'), 'r').read()
+vsre = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(vsre, verstrline, re.M)
+
+if mo:
+    __version__ = mo.group(1)
+else:
+    raise RuntimeError('Unable to find version string in "{}/__init__.py".'.format(__pkg_name__))
+
+dir_path = os.path.dirname(__file__)
+with open(os.path.join(dir_path, 'requirements.txt')) as fh:
+    install_requires = [
+        str(requirement) for requirement in
+        pkg_resources.parse_requirements(fh)]
+
+data_files = []
+extra_requires = {}
+extensions = []
+
 setup(
     name='pychopper',
-    version='2.7.0',
+    version= __version__,
     description="A tool to identify full length cDNA reads.",
     long_description=readme,
     author="ONT Applications Group",
